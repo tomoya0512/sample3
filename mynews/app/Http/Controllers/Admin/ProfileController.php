abbb6624a85dd4f521c7a17c65e08b7f31fbefb2
EC2 Default User <ec2-user@ip-172-31-42-24.ap-northeast-1.compute.internal>
2021-11-08 13:29:35 +0000

    Lalavel課題4

--- mynews/app/Http/Controllers/Admin/ProfileController.php
+++ mynews/app/Http/Controllers/Admin/ProfileController.php
@@ -7,19 +7,23 @@ use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
-    public function add(){
-        return view('admin.profile.add');
+    public function add()
+    {
+        return view('admin.profile.create');
     }
 
-    public function create(){
-        return redirect('admin.profile.create');
+    public function create()
+    {
+        return redirect('admin/profile/create');
     }
 
-    public function edit(){
+    public function edit()
+    {
         return view('admin.profile.edit');
     }
 
-    public function update(){
-        return redirect('admin.profile.updata');
+    public function update()
+    {
+        return redirect('admin/profile/edit');
     }
 }
--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-    Route::get('profile/add','Admin\ProfileController@add');
+    Route::get('profile/create','Admin\ProfileController@add');
     Route::get('profile/edit','Admin\ProfileController@edit');
 });
 
