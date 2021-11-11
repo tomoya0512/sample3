907827bfc169d102af94985bf0aba0723c45d222
EC2 Default User <ec2-user@ip-172-31-42-24.ap-northeast-1.compute.internal>
2021-11-11 09:03:57 +0000

    lalavel課題6

--- mynews/app/Http/Controllers/Admin/NewsController.php
+++ mynews/app/Http/Controllers/Admin/NewsController.php
@@ -11,4 +11,9 @@ class NewsController extends Controller
     public function add(){
     return view('admin.news.create');
     }
+    
+    public function create(Request $request){
+      // admin/news/createにリダイレクトする
+      return redirect('admin/news/create');
+    } 
 }
--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -16,9 +16,12 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');;
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
     Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
+    Route::post('profile/create','Admin\ProfileController@create')->middleware('auth');
     Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/edit','Admin\ProfileController@updata')->middleware('auth');
 });
 
 
